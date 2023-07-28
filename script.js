746e31817138801369475607de457d50d4ac1b49
Nick Burr <nicholasstclairburr@gmail.com>
2023-07-27 21:33:24 -0600

    started the fetch call for GIPHY, there is an error 401 in the console, need to figure out!

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var animalAPI = "FsdXAP6AmCh3sGYdfhvA6Q==WYbgbcTXlNXxZkxm";
 var tenorAPI = "AIzaSyDEa3l0bqCEAeClPPwFIpniGKyPjJg2VRw";
+var giphyAPI = `iNJblv33ezpXseWc2SeIxWyVYNY9QCc5`
 var animalSearch = document.querySelector("#startbtn");
+var animalName = 'giraffe'
+var giphyURL =`https://api.giphy.com/v1/gifs/trending?q:${animalName}?api_key:${giphyAPI}`
+var animalURL =  `https://api.api-ninjas.com/v1/animals?name= ${animalName}`; 
 function getApi() {
 // var name = document.getElementById('initialSearch')
-var name = 'giraffe'
-var reqURL =  `https://api.api-ninjas.com/v1/animals?name= ${name}`; 
-fetch (reqURL, {
+fetch (animalURL, {
     headers: {"X-Api-Key": animalAPI}
   })
     .then(function (response){
@@ -14,6 +16,14 @@ fetch (reqURL, {
     .then(function (data){
         console.log(data)
 })
+
+fetch(giphyURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
 
 };
 
