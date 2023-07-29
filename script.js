ea38f5013f897d50e271555854904e86254821c5
saevans86 <sam_evans1986@hotmail.com>
2023-07-29 11:39:08 -0600

    combined search function to api url string, added ul in html

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //var animalAPI = "FsdXAP6AmCh3sGYdfhvA6Q==WYbgbcTXlNXxZkxm";
 //var tenorAPI = "AIzaSyDEa3l0bqCEAeClPPwFIpniGKyPjJg2VRw";
 var animalSearch = document.querySelector("#startbtn");
+var initialUserInput = document.getElementById("initialUserInput")
+var name = " ";
+var animalFactBox =  document.getElementById("animalFacts")
+
 function getApi() {
-    //var name = 
-  var reqURL =  'https://api.api-ninjas.com/v1/animals?name=' + "cheetah"; 
+  var searchValue = initialUserInput.value;
+  var reqURL =  'https://api.api-ninjas.com/v1/animals?name=' + searchValue; 
   fetch (reqURL, {
     headers: {"X-Api-Key": "FsdXAP6AmCh3sGYdfhvA6Q==WYbgbcTXlNXxZkxm"}
   })
@@ -11,12 +15,23 @@ function getApi() {
 return response.json();
 })
 .then(function (data){
-    console.log(data)
+
+  console.log(data)
+  var contentHtml = `
+  <li>Habitat: ${data[0].characteristics.habitat} </li>
+  <li>Diet: ${data[0].characteristics.diet}</li>
+  <li>Locations: ${data[0].locations[0]} </li>
+  <li>Predators: ${data[0].characteristics.predators} </li>
+  `
+animalFactBox.innerHTML = contentHtml
+
 })
 
 };
 
-getApi();
-//animalSearch.addEventListener("click", function());
+
+animalSearch.addEventListener("click", function() {
+  getApi();
+})
 
 //renderGifs()
